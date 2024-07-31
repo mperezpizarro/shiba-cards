5bf51b3bf108b7b76732c7b6317b69a0611f4073
mperezpizarro <marioperezpizarro@gmail.com>
2024-07-31 10:15:26 +0200

    Added info message

--- src/App.vue
+++ src/App.vue
@@ -5,27 +5,27 @@ import Link from './components/Link.vue';
 import Card from './components/Card.vue';
 import Loading from './components/Loading.vue';
 
-const data = ref(null)
+const data = ref("Placeholder") //OG Value (null)
 const loading = ref(true)
+const fetched = ref(false)
 
-fetch("https://shibe.online/api/shibes?count=1&httpsUrls=true")
+/*fetch("https://shibe.online/api/shibes?count=1&httpsUrls=true")
   .then(res => res.json())
   .then(value => {
     data.value = value
     loading.value = false
   })
 
-const fetched = ref(false)
 
 function handleClick() {
   fetched.value = true
-}
+}*/
 </script>
 
 <template>
   <main class="w-full min-h-screen flex items-center justify-center flex-col px-4 py-2">
     <h1 class="text-3xl font-bold mt-4">Shiba Cards</h1>
-    <h2 class="text-xl mt-2">Powered by <Link href="https://shibe.online/" title="API">shibe.online</Link> API</h2>
+    <h2 class="text-xl text-center mt-2" style="text-wrap: balance;">The shibe.online API no longer exists. This website not functional anymore.</h2>
     <Loading v-show="loading" />
     <Card v-show="!loading" :onClick="handleClick" :fetched="fetched" :href="data" />
     <section class="mt-6 mb-4 flex flex-col gap-4 text-center">
